48d4e712a534733e8c155eb8d26c8d072d434641
franco <franco1.alfra@gmail.com>
2022-04-30 20:59:26 -0500

    Creacion carpetas MVC

--- app/src/main/java/pm/pe/edu/ulima/ocholocos/views/MainActivity.kt
+++ app/src/main/java/pm/pe/edu/ulima/ocholocos/views/MainActivity.kt
@@ -1,14 +1,12 @@
-package pm.pe.edu.ulima.ocholocos
+package pm.pe.edu.ulima.ocholocos.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pm.pe.edu.ulima.ocholocos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    //asdjaslkdjaslkdajsdkl
-    //dsadas
-    //sadasdasdadaadad
 }
