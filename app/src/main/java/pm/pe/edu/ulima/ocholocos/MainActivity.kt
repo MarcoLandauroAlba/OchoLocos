648b5d78d955949779b67634e899d636dd88162d
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-04-30 20:26:31 -0500

    Inicio

---
+++ app/src/main/java/pm/pe/edu/ulima/ocholocos/MainActivity.kt
@@ -0,0 +1,11 @@
+package pm.pe.edu.ulima.ocholocos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
