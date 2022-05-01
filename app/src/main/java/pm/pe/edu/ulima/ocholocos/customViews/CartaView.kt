295c9e9e5821c63d7dab5a4918bdb9228004c161
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-05-01 16:01:00 -0500

    PasoPaloPorAtributo

--- app/src/main/java/pm/pe/edu/ulima/ocholocos/customViews/CartaView.kt
+++ app/src/main/java/pm/pe/edu/ulima/ocholocos/customViews/CartaView.kt
@@ -1,11 +1,12 @@
 package pm.pe.edu.ulima.ocholocos.customViews
 
-import android.R
+
 import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import pm.pe.edu.ulima.ocholocos.R
 
 class CartaView : View{
     private val mPaint : Paint = Paint()
@@ -14,18 +15,22 @@ class CartaView : View{
     private var mSize : Float = 0f
     private var mWidth : Float = 0f
     private var mHeight : Float = 0f
+    private var paloCarta : Int = 0
 
     constructor(context: Context, attrs : AttributeSet): super(context, attrs){
-
+        val a : TypedArray = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CartaView,
+            0,
+            0
+        )
+        paloCarta = a.getInt(R.styleable.CartaView_PaloCarta, 4)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         mWidth = View.MeasureSpec.getSize(widthMeasureSpec).toFloat()
         mHeight = View.MeasureSpec.getSize(heightMeasureSpec).toFloat()
-
-        mSize = Math.min(mWidth, mHeight)
-
         setMeasuredDimension(mWidth.toInt(),mHeight.toInt())
     }
 
@@ -33,26 +38,31 @@ class CartaView : View{
         super.onDraw(canvas)
         canvas!!.drawColor(Color.WHITE)
         drawCarta(canvas!!)
-        drawPalo(canvas!!)
+        drawPalo(canvas!!, paloCarta)
 
     }
     private fun drawCarta(canvas : Canvas){
-        val lado = mWidth / 2f
         mPaint.color = Color.BLACK
         mPaint.style = Paint.Style.STROKE
         canvas!!.drawRect(0f,mHeight,mWidth,0f, mPaint)
     }
-    private fun drawPalo(canvas : Canvas){
+    private fun drawPalo(canvas : Canvas, palo : Int){
         mPaintN.color = Color.BLACK
         mPaintN.style = Paint.Style.FILL_AND_STROKE
         mPaintR.color = Color.RED
         mPaintR.style = Paint.Style.FILL_AND_STROKE
-        mPaintR.textSize = 500f
+        mPaintR.textSize = mWidth / 2f
         mPaintN.textSize = mWidth / 1.5f
-        //canvas!!.drawRect(lado / 2f,mHeight / 2f + mSize / 4f,mWidth - mWidth / 4f,mHeight/3f, mPaint)
-        //canvas!!.drawText("♤",mWidth / 2f -  mWidth / 4f, mHeight / 2f +  mHeight / 6.3f, mPaintN)
-        canvas!!.drawText("♧",mWidth / 2f -  mWidth / 3.3f, mHeight / 2f +  mHeight / 6f, mPaintN)
-        //canvas!!.drawText("♥",mWidth / 6f + 50f, mHeight/2f + 150f, mPaintR)
-        //canvas!!.drawText("♦",mWidth / 6f + 50f, mHeight/2f + 150f, mPaintR)
+        if(palo == 0){
+            canvas!!.drawText("♥",mWidth / 2f -  mWidth / 3.2f, mHeight / 2f +  mHeight / 10f, mPaintR)
+        }else if(palo == 1){
+            canvas!!.drawText("♥",mWidth / 2f -  mWidth / 3.2f, mHeight / 2f +  mHeight / 10f, mPaintR)
+        }else if(palo == 2){
+            canvas!!.drawText("♦",mWidth / 2f -  mWidth / 3.1f, mHeight / 2f +  mHeight / 10f, mPaintR)
+        }else if(palo == 3){
+            canvas!!.drawText("♧",mWidth / 2f -  mWidth / 3.3f, mHeight / 2f +  mHeight / 6.8f, mPaintN)
+        }else if(palo == 4){
+            canvas!!.drawText("♤",mWidth / 2f -  mWidth / 4f, mHeight / 2f +  mHeight / 6.3f, mPaintN)
+        }
     }
 }
