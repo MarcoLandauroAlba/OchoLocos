e08d1fb9f4336b1a8135342548f6f4ab40757415
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-05-01 14:40:27 -0500

    Avance Custom views

--- app/src/main/java/pm/pe/edu/ulima/ocholocos/customViews/CartaView.kt
+++ app/src/main/java/pm/pe/edu/ulima/ocholocos/customViews/CartaView.kt
@@ -1,28 +1,22 @@
 package pm.pe.edu.ulima.ocholocos.customViews
 
+import android.R
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
-import pm.pe.edu.ulima.ocholocos.R
 
 class CartaView : View{
     private val mPaint : Paint = Paint()
+    private val mPaintN : Paint = Paint()
+    private val mPaintR : Paint = Paint()
     private var mSize : Float = 0f
     private var mWidth : Float = 0f
     private var mHeight : Float = 0f
 
-    constructor(context : Context) : super(context){
-        //Crear una instancia con solo codigo Kotlin
-    }
     constructor(context: Context, attrs : AttributeSet): super(context, attrs){
-        //Crear una instancia a partir de XML
-//        este es otro cambio
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -34,14 +28,31 @@ class CartaView : View{
 
         setMeasuredDimension(mWidth.toInt(),mHeight.toInt())
     }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas!!.drawColor(Color.WHITE)
         drawCarta(canvas!!)
+        drawPalo(canvas!!)
+
     }
     private fun drawCarta(canvas : Canvas){
         val lado = mWidth / 2f
         mPaint.color = Color.BLACK
         mPaint.style = Paint.Style.STROKE
         canvas!!.drawRect(0f,mHeight,mWidth,0f, mPaint)
+    }
+    private fun drawPalo(canvas : Canvas){
+        mPaintN.color = Color.BLACK
+        mPaintN.style = Paint.Style.FILL_AND_STROKE
+        mPaintR.color = Color.RED
+        mPaintR.style = Paint.Style.FILL_AND_STROKE
+        mPaintR.textSize = 500f
+        mPaintN.textSize = mWidth / 1.5f
+        //canvas!!.drawRect(lado / 2f,mHeight / 2f + mSize / 4f,mWidth - mWidth / 4f,mHeight/3f, mPaint)
+        //canvas!!.drawText("♤",mWidth / 2f -  mWidth / 4f, mHeight / 2f +  mHeight / 6.3f, mPaintN)
+        canvas!!.drawText("♧",mWidth / 2f -  mWidth / 3.3f, mHeight / 2f +  mHeight / 6f, mPaintN)
+        //canvas!!.drawText("♥",mWidth / 6f + 50f, mHeight/2f + 150f, mPaintR)
+        //canvas!!.drawText("♦",mWidth / 6f + 50f, mHeight/2f + 150f, mPaintR)
     }
 }
